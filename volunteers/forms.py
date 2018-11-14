03c11d1245cc687c3b16c9b47d9cc630b57eb25f
azamatmurdalov <azamat.murdalov@mail.ru>
2018-11-14 21:16:27 +0300

    edit html form

--- volunteers/forms.py
+++ volunteers/forms.py
@@ -3,7 +3,6 @@ from django import forms
 from .models import Volunteer
 
 class PostForm(forms.ModelForm):
-
     class Meta:
         model = Volunteer
         fields = ('name', 'phone', 'give')
--- volunteers/models.py
+++ volunteers/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
+TITLE_CHOICES = (
+	('Событийный волонтер', False),
+	('Волонтер-донор', False),
+	('Волонтер общественной безопасности', False),
+	('Медиа-волонтер', False),
+	('Волонтер помощи страдающим паническими атаками', False),
+	('Волонтеры финансовой помощи', False),
+)
+
 class Volunteer(models.Model):
 	name = models.CharField(max_length = 120)
 	phone = models.CharField(max_length = 120)
-	give = models.TextField()
+	give = models.CharField(max_length=120, choices=TITLE_CHOICES)
 	date = models.DateTimeField(auto_now_add = True)
 
 	def __str__(self):
